09fc3b1dc81f5ccf84d2190ff1dc2811f0267470
Irgey <116591589+Irgey@users.noreply.github.com>
2023-06-16 18:46:58 +0200

    alt for img added

--- src/components/BookDetails/BooksDetails.jsx
+++ src/components/BookDetails/BooksDetails.jsx
@@ -1,18 +1,18 @@
-import { useQuery } from "@tanstack/react-query";
-import TableCell, { tableCellClasses } from "@mui/material/TableCell";
-import TableRow from "@mui/material/TableRow";
-import s from "../../pages/styles.module.css";
-import { getFullInfoById } from "../../services/books-api";
-import { ReactMarkdown } from "react-markdown/lib/react-markdown";
-import rehypeRaw from "rehype-raw";
-import { styled } from "@mui/material";
-import { useSearchParams } from "react-router-dom";
+import { useQuery } from '@tanstack/react-query';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
+import s from '../../pages/styles.module.css';
+import { getFullInfoById } from '../../services/books-api';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
+import rehypeRaw from 'rehype-raw';
+import { styled } from '@mui/material';
+import { useSearchParams } from 'react-router-dom';
 
 export const BooksDetails = () => {
   const [searchParams] = useSearchParams();
-  const bookId = searchParams.get("details");
+  const bookId = searchParams.get('details');
   const { data: detailedData } = useQuery({
-    queryKey: ["bookInfo", bookId],
+    queryKey: ['bookInfo', bookId],
     queryFn: () => getFullInfoById(bookId),
     keepPreviousData: true,
   });
@@ -31,15 +31,18 @@ export const BooksDetails = () => {
               </h2>
               <div className={s.contentWrapper}>
                 <div>
-                  {" "}
-                  <img src={detailedData.volumeInfo.imageLinks.thumbnail} />
+                  {' '}
+                  <img
+                    alt={detailedData.volumeInfo.title}
+                    src={detailedData.volumeInfo.imageLinks.thumbnail}
+                  />
                 </div>
                 <ul className={s.contentList}>
                   {detailedData.volumeInfo.authors && (
                     <li>
                       <p>
                         <b>Author: </b>
-                        {detailedData.volumeInfo.authors.join(", ")}
+                        {detailedData.volumeInfo.authors.join(', ')}
                       </p>
                     </li>
                   )}
@@ -70,14 +73,14 @@ export const BooksDetails = () => {
                   )}
                   {detailedData.volumeInfo.description && (
                     <li>
-                      {" "}
+                      {' '}
                       <b>Description: </b>
                       <ReactMarkdown
                         className={s.descrContainer}
                         rehypePlugins={[rehypeRaw]}
                       >
                         {detailedData.volumeInfo.description}
-                      </ReactMarkdown>{" "}
+                      </ReactMarkdown>{' '}
                     </li>
                   )}
                 </ul>
@@ -101,11 +104,11 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
 }));
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  "&:nth-of-type(odd)": {
+  '&:nth-of-type(odd)': {
     backgroundColor: theme.palette.action.hover,
   },
   // hide last border
-  "&:last-child td, &:last-child th": {
+  '&:last-child td, &:last-child th': {
     border: 0,
   },
 }));
