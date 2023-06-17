0a4e12355194a5cf6bbf3b6c04344d97710223ab
Irgey <116591589+Irgey@users.noreply.github.com>
2023-06-17 22:42:18 +0200

    imports removed

--- src/pages/BooksPage.jsx
+++ src/pages/BooksPage.jsx
@@ -1,11 +1,7 @@
 // Libs
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { useSearchParams } from 'react-router-dom';
-import React, { useEffect, useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
 // Mui components
 // Local
-import { searchByQuery } from '../services/books-api';
 import { BooksTable } from 'components/BooksTable/BooksTable';
 
 export const BooksPage = () => {
