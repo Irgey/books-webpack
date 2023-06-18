c3927203224827287c5c2805370c7b961a27d9e6
Irgey <116591589+Irgey@users.noreply.github.com>
2023-06-18 12:40:50 +0200

    env added

--- src/services/books-api.js
+++ src/services/books-api.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
+import { MAX_RESULTS } from 'constants';
 
-const BASE_URL = 'https://www.googleapis.com/books/v1/volumes';
-const API_KEY = 'AIzaSyC39PA06hIM4P6RXk4tFDzFihjHVUO8cNw';
-const MAX_RESULTS = 40;
 export const searchByQuery = async query => {
   const queryParams = query.trim().split(' ').join('+');
   const { data } = await axios.get(
-    `${BASE_URL}/?q=${queryParams}&langRestrict=en&maxResults=${MAX_RESULTS}&key=${API_KEY}`
+    `${process.env.REACT_APP_BASE_URL}/?q=${queryParams}&langRestrict=en&maxResults=${MAX_RESULTS}&key=${process.env.REACT_APP_API_KEY}`
   );
   return data;
 };
 export const getFullInfoById = async id => {
-  const { data } = await axios.get(`${BASE_URL}/${id}?key=${API_KEY}`);
+  const { data } = await axios.get(
+    `${process.env.REACT_APP_BASE_URL}/${id}?key=${process.env.REACT_APP_API_KEY}`
+  );
   return data;
 };
